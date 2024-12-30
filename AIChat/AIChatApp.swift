2ff48ecfa02b2daaed43821fc86b5c953c398285
Andrew McLean <mclean14@gmail.com>
2024-12-30 09:33:31 -0700

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Andrew McLean on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatTests/AIChatTests.swift
@@ -0,0 +1,16 @@
+//
+//  AIChatTests.swift
+//  AIChatTests
+//
+//  Created by Andrew McLean on 12/30/24.
+//
+
+import Testing
+
+struct AIChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
